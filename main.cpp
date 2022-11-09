612c7b7675242e31d21a4a75ecc0a5a7a412842a
Z1R0Axcel <113200832+Z1R0Axcel@users.noreply.github.com>
2022-11-08 18:53:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-// write your code here
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  double a, b, c, discriminant;
+  cout << "Enter a (the coefficient of x^2): ";
+  cin >> a;
+  cout << "Enter b (the coefficient of x): ";
+  cin >> b;
+  cout << "Enter c (the constant term): ";
+  cin >> c;
+  discriminant = pow(b, 2.0) - 4 * a * c;
+  if (discriminant == 0) {
+    cout << "Single (repeated) root." << endl;
+    cout << "x = " << -b / (2.0 * a) << endl;
+  } else if (discriminant > 0) {
+    cout << "Two real roots." << endl;
+    cout << "x1 = " << (-b + pow(discriminant, 1.0 / 2.0)) / (2.0 * a) << endl
+         << "x2 = " << (-b - pow(discriminant, 1.0 / 2.0)) / (2.0 * a) << endl;
+  } else
+    cout << "Two complex roots." << endl;
+  return 0;
+}
