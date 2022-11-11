2043331d0c366e050cdf85ffabc052fdaedca94f
Z1R0Axcel <113200832+Z1R0Axcel@users.noreply.github.com>
2022-11-11 10:58:57 -0600

    Page 259

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@
 using namespace std;
 int main() {
   double a, b, c, discriminant;
-  cout << "Enter a (the coefficient of x^2): ";
+  cout << "Enter a (the coefficient of x^2), enter b (the coefficient of x), "
+          "enter c (the constant term):  ";
   cin >> a;
-  cout << "Enter b (the coefficient of x): ";
   cin >> b;
-  cout << "Enter c (the constant term): ";
   cin >> c;
   discriminant = pow(b, 2.0) - 4 * a * c;
   if (discriminant == 0) {
-    cout << "Single (repeated) root." << endl;
-    cout << "x = " << -b / (2.0 * a) << endl;
+    cout << "The signle root is" << -b / (2.0 * a) << endl;
   } else if (discriminant > 0) {
     cout << "Two real roots." << endl;
-    cout << "x1 = " << (-b + pow(discriminant, 1.0 / 2.0)) / (2.0 * a) << endl
-         << "x2 = " << (-b - pow(discriminant, 1.0 / 2.0)) / (2.0 * a) << endl;
+    cout << "-First root " << (-b + pow(discriminant, 1.0 / 2.0)) / (2.0 * a)
+         << endl
+         << "-Second root " << (-b - pow(discriminant, 1.0 / 2.0)) / (2.0 * a)
+         << endl;
   } else
     cout << "Two complex roots." << endl;
   return 0;
