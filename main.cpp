d6284b260be312e121fc70d88ca6ca85f6a6b7bc
Z1R0Axcel <113200832+Z1R0Axcel@users.noreply.github.com>
2022-11-14 11:43:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,16 @@ int main() {
   cin >> c;
   discriminant = pow(b, 2.0) - 4 * a * c;
   if (discriminant == 0) {
-    cout << "The signle root is" << -b / (2.0 * a) << endl;
+    cout << "The single root is " << -b / (2.0 * a) << endl;
   } else if (discriminant > 0) {
-    cout << "Two real roots." << endl;
+    cout << "Two real roots" << endl;
     cout << "-First root " << (-b + pow(discriminant, 1.0 / 2.0)) / (2.0 * a)
          << endl
          << "-Second root " << (-b - pow(discriminant, 1.0 / 2.0)) / (2.0 * a)
          << endl;
   } else
-    cout << "Two complex roots." << endl;
+    cout << "Complex roots" << endl;
+  else
+    cout << "Invalid entry" << endl;
   return 0;
 }
